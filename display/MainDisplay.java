465e6ffbf8f66c45c4652aed2d554b167803254f
Saurabh Totey <SaurabhTotey@gmail.com>
2017-03-26 22:44:36 -0600

    Made working beginning GUI

--- display/MainDisplay.java
+++ display/MainDisplay.java
@@ -3,38 +3,48 @@
  */
 package display;
 
-import java.awt.Color;
 import java.awt.Font;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
-import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
 import net.miginfocom.swing.MigLayout;
 
 /**
  * @author Saurabh Totey
- *
+ * This is the class for the main display where the game will be played
  */
 public class MainDisplay {
 	
+	/**
+	 * These are all of the core parts of the Main GUI
+	 */
 	public JFrame frame;
 	public JPanel mainPane;
 	public JPanel optionsPane;
 	public JPanel inventoryPane;
-	public JScrollPane logPane;
+	public JPanel logPane;
 	public JTextArea logArea;
-	
+	public JTextArea inputField;
 	public Font titleFont;
 	public Font defaultFont;
 	
+	/**
+	 * This is the constructor for the gui object
+	 * It makes a jframe with all of the necessary parts and makes it somewhat visually appealing
+	 * TODO this could use a LOT of work
+	 */
 	public MainDisplay(){
 		//This part makes the frame, sizes and centers it, and sets it's layout
 		this.frame = new JFrame("TOTEY RPG");
@@ -52,33 +62,77 @@ public class MainDisplay {
 		this.frame.setLocation(windowPosX, windowPosY);
 		this.frame.setLayout(new MigLayout());
 		
+		//This part makes the new fonts based on screen size
 		titleFont = new Font("titleFont", Font.ROMAN_BASELINE, screenX / 70);
-		defaultFont = new Font("defaultFont", Font.PLAIN, screenX / 80);
+		defaultFont = new Font("defaultFont", Font.PLAIN, screenX / 120);
 		
+		//This code makes two textareas, one for a log, and another for text input and configures them so that one displays info, and the other sends it to main
+		this.logPane = new JPanel(new MigLayout());
 		this.logArea = new JTextArea();
-		this.logPane = new JScrollPane(this.logArea);
+		this.logPane.add(new JScrollPane(this.logArea), "push, grow");
+		this.inputField = new JTextArea();
+		JScrollPane inputPane = new JScrollPane(this.inputField);
+		inputPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		this.logPane.add(inputPane, "push, grow, dock south");
 		this.logArea.setFont(defaultFont);
-		this.logArea.setFocusable(false);
-		this.logArea.setBackground(Color.WHITE);
+		this.logArea.setEditable(false);
+		this.logArea.setLineWrap(true);
 		this.frame.add(this.logPane, "dock south, height 25%!");
+		this.redirectSystemStreams();
 		
 		this.optionsPane = new JPanel();
 		JLabel optionsTitle = new JLabel("Options");
 		optionsTitle.setFont(titleFont);
 		optionsTitle.setHorizontalAlignment(SwingConstants.CENTER);
 		this.optionsPane.add(optionsTitle, "dock north");
-		this.frame.add(this.optionsPane, "dock west, width 25%!");
+		this.frame.add(this.optionsPane, "dock west, width 15%!");
 		
 		this.inventoryPane = new JPanel();
 		JLabel invTitle = new JLabel("Inventory");
 		invTitle.setFont(titleFont);
 		invTitle.setHorizontalAlignment(SwingConstants.CENTER);
 		this.inventoryPane.add(invTitle, "dock north");
-		this.frame.add(this.inventoryPane, "dock east, width 25%!");
+		this.frame.add(this.inventoryPane, "dock east, width 15%!");
 		
 		this.mainPane = new JPanel();
-		this.frame.add(this.mainPane, "grow, push, width 50%!, height 75%!");
+		this.frame.add(this.mainPane, "grow, push, width 70%!, height 75%!");
 		
 		this.frame.setVisible(true);
+		this.inputField.requestFocus();
 	}
+	
+	/**
+	 * This function just makes it so that all logged text goes to the GUI instead of the java log
+	 */
+	public void redirectSystemStreams(){
+		OutputStream out = new OutputStream(){
+			@Override
+			public void write(int b) throws IOException {
+				updateTextArea(String.valueOf((char) b));
+			}
+			@Override
+			public void write(byte[] b, int off, int len) throws IOException {
+				updateTextArea(new String(b, off, len));
+			}
+			@Override
+			public void write(byte[] b) throws IOException{
+				write(b, 0, b.length);
+			}
+		};
+		System.setOut(new PrintStream(out, true));
+		System.setErr(new PrintStream(out, true));
+	}
+	
+	/**
+	 * This adds text to the log area of the GUI
+	 * @param text text to be added to log area of GUI
+	 */
+	public void updateTextArea(final String text){
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				logArea.append(text);
+			}
+		});
+	}
+	
 }
--- main/Main.java
+++ main/Main.java
@@ -3,18 +3,89 @@
  */
 package main;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /**
  * @author Saurabh Totey
  * This class is the entry class of the program
+ * It is all stored in an object so that it can be serialized if needed
  */
 public class Main {
+	
+	/**
+	 * The main object is static so that it can be accessed from other classes
+	 */
+	public static Main main;
+	
+	/**
+	 * This is all the main object's instance data
+	 * This is where all of the core components of the game are kept
+	 */
+	public display.MainDisplay gui;
+	public boolean willInterpretIncoming = false;
+	public String uninterpretedText = "";
+	
+	/**
+	 * The main object
+	 * this holds the entire game in one object
+	 * this can be serialized and saved to later be resumed
+	 */
+	public Main() throws InterruptedException{
+		this.gui = new display.MainDisplay();
+		this.gui.inputField.addKeyListener(new KeyListener(){
+			@Override
+		    public void keyPressed(KeyEvent e){
+				if(e.getKeyCode() == KeyEvent.VK_ENTER){
+					e.consume();
+					getText();
+					gui.inputField.setText("");
+				}
+		    }
+		    @Override
+		    public void keyTyped(KeyEvent e) {
+		    }
+		    @Override
+		    public void keyReleased(KeyEvent e) {
+		    }
+		});
+		log("You wake up in a vast dank cave. Your head hurts, and you don't remember much. You try to remember your name. You think it might be");
+		while(this.uninterpretedText.isEmpty()){
+			Thread.sleep(15);
+		}
+		String name = uninterpretedText;
+		log("Yes, you remember that your name is " + name + ". Now that you figured that out, you look down at yourself, and are surprised to see that you are a");
+		this.uninterpretedText = "";
+	}
 
 	/**
 	 * This is the entry point for the program
-	 * @param args default main paramaters
+	 * @param args default main parameters
 	 */
-	public static void main(String[] args) {
-		display.MainDisplay gui = new display.MainDisplay();
+	public static void main(String[] args) throws InterruptedException{
+		main = new Main();
+	}
+	
+	/**
+	 * This function logs messages with timestamps
+	 * @param message the message that is to be logged
+	 */
+	public static void log(String message){
+		System.out.println("[" + new SimpleDateFormat("dd/MM/yy HH:mm:ss").format(Calendar.getInstance().getTime()) + "] : " + message);
+	}
+	
+	/**
+	 * This function handles all incoming text by either saving it as instancedata or by attempting to interpret commands
+	 * @param incoming the text to either save or interpret
+	 */
+	public void getText(){
+		if(this.willInterpretIncoming){
+			//TODO interpret incoming text
+		}else{
+			this.uninterpretedText = this.gui.inputField.getText();
+		}
 	}
 
 }
